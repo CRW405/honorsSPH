d5d5535e76049bfa11616b95e40c6a5d1755c4c4
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-09-23 01:08:15 -0500

    rewrote the particle system to be np array based, speeds things up by ~20 fps

--- src/main.py
+++ src/main.py
@@ -14,31 +14,41 @@ p_texture: Texture2D
 p_source: Rectangle
 p_origin: Vector2
 
+def create_particle_texture() -> None:
+    global p_texture, p_source, p_origin
+    p_image: Image = gen_image_color(1, 1, WHITE)
+    p_texture = load_texture_from_image(p_image)
+    unload_image(p_image)
+    p_source = Rectangle(0, 0, 1, 1)
+    p_origin = Vector2(0.5, 0.5)
+
 class Particles:
     positions: np.ndarray
     velocities: np.ndarray
     densities: np.ndarray
     pressures: np.ndarray
+    color: Color
 
-    def __init__(self) -> None:
+    def __init__(self, color:Color=BLUE) -> None:
         self.positions = np.empty((0, 2))
         self.velocities = np.empty((0, 2))
         self.densities = np.empty(0)
         self.pressures = np.empty(0)
+        self.color = color
 
-    def add(self, position: np.ndarray, density: float, velocity: np.ndarray, pressure: float) -> None:
-        self.positions = np.append(self.positions, position)
-        self.velocities = np.append(self.velocities, velocity)
+    def add(self, position: np.ndarray, velocity: np.ndarray, density: float, pressure: float) -> None:
+        self.positions = np.vstack([self.positions, position])
+        self.velocities = np.vstack([self.velocities, velocity])
         self.densities = np.append(self.densities, density)
         self.pressures = np.append(self.pressures, pressure)
 
     def create_random_particles(self, amount: int) -> None:
         self.positions = np.random.rand(amount, 2) * np.array([screen_width, screen_height])
-        self.velocities = np.zeros((amount, 2))
+        self.velocities = np.random.rand(amount, 2) * 50 - 25
         self.densities = np.zeros(amount)
         self.pressures = np.zeros(amount)
 
-    def render(self) -> None:
+    def render_textures(self) -> None:
         for i in range(len(self.positions)):
             draw_texture_pro(p_texture,
                              p_source,
@@ -48,30 +58,35 @@ class Particles:
                                        p_size),
                              p_origin,
                              0.0,
-                             WHITE)
+                             self.color)
 
+    def update(self, dt: float) -> None:
+        self.velocities[:, 1] += g * dt
 
-def create_particle_texture() -> None:
-    global p_texture, p_source, p_origin
-    p_image: Image = gen_image_color(1, 1, WHITE)
-    p_texture = load_texture_from_image(p_image)
-    unload_image(p_image)
-    p_source = Rectangle(0, 0, 1, 1)
-    p_origin = Vector2(0.5, 0.5)
+        self.positions += self.velocities * dt
 
-# def draw_particle(particle: Particle) -> None:
-#     draw_texture_pro(p_texture,
-#                      p_source,
-#                      Rectangle(particle.position[0] - p_size / 2,
-#                                particle.position[1] - p_size / 2,
-#                                p_size,
-#                                p_size),
-#                      p_origin,
-#                      0.0,
-#                      particle.color)
+        # floor collision
+        floor = self.positions[:, 1] > screen_height - p_size / 2
+        self.positions[floor, 1] = screen_height - p_size / 2
+        self.velocities[floor, 1] *= -0.5  # bounce
+
+        # ceiling collision
+        ceiling = self.positions[:, 1] < p_size / 2
+        self.positions[ceiling, 1] = p_size / 2
+        self.velocities[ceiling, 1] *= -0.5
+
+        # right wall collision
+        right = self.positions[:, 0] > screen_width - p_size / 2
+        self.positions[right, 0] = screen_width - p_size / 2
+        self.velocities[right, 0] *= -0.5
+
+        # left wall collision
+        left = self.positions[:, 0] < p_size / 2
+        self.positions[left, 0] = p_size / 2
+        self.velocities[left, 0] *= -0.5
 
 target_fps: int = 60
-speedup:float = 1.0 # I didnt wanna wait to see my results
+speedup:float = 10.0
 random_partcle_amount: int = 10000
 
 async def main() -> None:
@@ -85,6 +100,12 @@ async def main() -> None:
     particles = Particles()
     particles.create_random_particles(random_partcle_amount)
 
+    single = Particles(color=RED)
+    single.add(np.array([[screen_width / 2, screen_height / 2]]),
+               np.array([10.0, 10.0]),
+               1.0,
+               1.0)
+
     # main loop
     while not window_should_close():
         dt: float = get_frame_time() * speedup
@@ -93,7 +114,11 @@ async def main() -> None:
 
         # this is where we do real work
         draw_text("Hello World", 200, 200, 20, BLACK)
-        particles.render()
+        particles.update(dt)
+        particles.render_textures()
+
+        single.update(dt)
+        single.render_textures()
 
         draw_text(f"FPS: {get_fps()}", 10, 10, 20, BLACK)
         end_drawing()

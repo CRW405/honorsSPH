ca6dcce64cf7d1e47bac2dd8c90b3cfa6bdf323b
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-09-22 19:49:14 -0500

    reorganizes project structure, updates README, sets up a venv and dependencies, main has a simple particle system now - its all placeholder but hopefully helps some

--- README.md
+++ README.md
@@ -5,65 +5,36 @@
 
 A 2D SPH fluid sim for a physics honors contract.
 
+## Tech
+
+- Python
+- Numpy
+- Scipy
+- Raylib
+
 ## Goals
 
-- Real time or pre rendered fluid sim
-- Interactivity of some sort such as defining liquid spawn, customizable variables, etc
-- Visual impact > technical impressiveness due to audience
+- Real time 2D SPH fluid sim using particles for presentation.
+- Model: Density, Fluidity, Pressure.
+- Spawn liquid with a click.
+- Ability to adjust: Density, Fluidity, Pressure
+
+## Organization
+
+- Git issues as a tasklist.
+- Every person must complete one task per round.
+
+## Contributors
+
+* Bryan - Project Lead
+* Caleb
+* Symphony
+
+## Resources
+
+- [Numpy](https://numpy.org/])
+- [Scipy](https://scipy.org/)
+- [Raylib](https://www.raylib.com/)
+- [Raylib Python Bindings](https://electronstudio.github.io/raylib-python-cffi/)
 
 ## Notes
-
-### Personal Concerns
-
-- This is a difficult and ambitious project
-- I do not have the bandwidth to do a majority of work, workload needs to split evenly
-- < 2 months is not a lot of time
-- Reliance on AI will cripple us, all code needs to be written by you.
-- This project requires a good understanding of the whole codebase, theory, and implementation
-- I get ahead of myself too easily and need to be told no. I would be a bad team lead for this
-
-### Decision point
-
-We need to decide on a tech stack now.
-
-#### C path
-
-display, input, and, graphics stuff handled by raylib
-
-##### Pros
-
-- I personally like C
-- C is very fast
-- Data and CPU oriented programming is perfect for this use case
-- You learn C and low level programming
-- From scratch
-
-##### Cons
-
-- You two would need to learn:
-    - C
-    - data oriented programming
-    - Compiled language basics
-    - Cmake - I can probably set this up for us
-    - Raylib
-- C does not even have strings, dynamic arrays, other things you probably expect a language to have
-- C does not hold you hand at all
-
-#### Python path
-
-We could use:
-- Raylib - for real time
-- MatplotLib - for pre rendered
-- PyGame - I dont like PyGame but it is an option
-- Taichi Lang - faster code
-
-##### Pros
-
-- Quicker development turnaround
-- Access to OOP
-- Holds your hand way more and provides more libraries and tooling
-
-##### Cons
-
-- slower
-- More reliance on third party code
---
+++ src/main.py
@@ -0,0 +1,84 @@
+import numpy as np
+import scipy as sp
+import asyncio
+import platform
+from pyray import *
+from typing import *
+
+global screen_width:int = 500
+global screen_height:int = 500
+global g:float = 9.81
+global p_size:int = 10
+
+class Particle:
+    def __init__(self, position, density, velocity, pressure, color=BLUE):
+        self.position = position
+        self.density = density
+        self.velocity = velocity
+        self.pressure = pressure
+        self.color = color
+
+global p_texture, p_source, p_origin
+
+def create_particle_texture()->None:
+    global p_texture, p_source, p_origin
+    p_image = gen_image_color(1,1, WHITE)
+    p_texture = load_texture_from_image(p_image)
+    unload_image(p_image)
+    p_source = Rectangle(0,0,1,1)
+    p_origin = Vector2(0.5,0.5)
+
+def draw_particle(particle: Particle)->None:
+    draw_texture_pro(p_texture,
+                     p_source,
+                     Rectangle(particle.position[0] - p_size/2,
+                               particle.position[1] - p_size/2,
+                               p_size,
+                               p_size),
+                     p_origin,
+                     0.0,
+                     particle.color)
+
+# Placeholder !!!!
+def apply_forces(particle: Particle, dt: float)->None:
+    # Apply gravity
+    particle.velocity[1] += g * dt
+
+    # Update position based on velocity
+    particle.position += particle.velocity * dt
+
+    # bounds checking
+    if particle.position[1] > screen_height - p_size/2:
+        particle.position[1] = screen_height - p_size/2
+    if particle.position[1] < p_size/2:
+        particle.position[1] = p_size/2
+    if particle.position[0] > screen_width - p_size/2:
+        particle.position[0] = screen_width - p_size/2
+
+
+async def main():
+    init_window(screen_width, screen_height,"Honors SPH Fluid Sim")
+    fps:int = 60
+    set_target_fps(fps)
+    create_particle_texture()
+
+    test:Particle = Particle(np.array([screen_width/2, screen_height/2]),
+                             1.0,
+                             np.array([50.0, -25.0]),
+                             1.0)
+
+    while not window_should_close():
+        begin_drawing()
+        clear_background(WHITE)
+
+        draw_text("Hello World", 200, 200, 20, BLACK)
+
+        apply_forces(test, 1/fps)
+        draw_particle(test)
+
+        end_drawing()
+        await asyncio.sleep(0)
+    close_window()
+
+if __name__ == "__main__":
+    asyncio.run(main())
